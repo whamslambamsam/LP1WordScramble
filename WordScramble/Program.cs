c27321ee053099359718bf4fb7d3ebbe99bc6611
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-04-11 16:21:52 +0100

    Added project WordScramble to the solution

---
+++ WordScramble/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordScramble
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
